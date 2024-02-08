5061840d2c3e43a37d895946101501925e72bfd1
yeliztacgin <145569090+yeliztacgin@users.noreply.github.com>
2024-02-08 21:13:13 +0300

    Add files via upload

---
+++ Baloncuklar/Script1.js
@@ -0,0 +1,17 @@
+function baloncuklar() {
+    const container = document.querySelector(".container");
+    const baloncuk = document.createElement("span");
+    const boyut = Math.random() * 100;
+    baloncuk.style.width = boyut + "px";
+    baloncuk.style.height = boyut + "px";
+    baloncuk.style.left = Math.random() * innerWidth + "px";
+
+    container.appendChild(baloncuk);
+    setTimeout(() => {
+        baloncuk.remove();
+
+
+    }, 2000);
+
+}
+setInterval(baloncuklar, 50);    
